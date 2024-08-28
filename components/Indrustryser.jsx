1cf88ffcb3a42b2df11611d79765536b69b5cc23
shahisrail <shahisrail134@gmail.com>
2024-08-28 22:41:52 +0600

    changes

--- components/Indrustryser.jsx
+++ components/Indrustryser.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import healthcare from "@/public/healthcare.png"
 import ecommerce from "@/public/ecommerce.png";
+import education from "@/public/education.png";
 
 const Indrustryser = () => {
   return (
@@ -12,103 +13,55 @@ const Indrustryser = () => {
         <p>Bridge Your Business with the Virtual World</p>
       </div>
       <div className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 gap-2">
-  <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
-    <div className="flex justify-center">
-    <Image src={healthcare} alt="healthcare" placeholder="blur" />
-    </div>
-    <div className="text-lg font-semibold">Healthcare</div>
-  </div>
-
-  <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
-    <div className="flex justify-center">
-      <Image 
-        src={ecommerce}
-        placeholder="blur" 
-        alt="E-Commerce" 
-
-      />
-    </div>
-    <div className="text-lg font-semibold">E-Commerce</div>
-  </div>
-
-  <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
-    <div className="flex justify-center">
-      <Image 
-       src="../assets/indeustry/Education.png" 
-        placeholder="blur" 
-        alt="Education" 
-        width={200} 
-        height={200} 
-      />
-    </div>
-    <div className="text-lg font-semibold">Education</div>
-  </div>
-
-  <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
-    <div className="flex justify-center">
-      <Image 
-        src="../assets/indeustry/real-estate.png" 
-        placeholder="blur" 
-        alt="Real-Estate" 
-        width={200} 
-        height={200} 
-      />
-    </div>
-    <div className="text-lg font-semibold">Real-Estate</div>
-  </div>
-
-  <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
-    <div className="flex justify-center">
-      <Image 
-         src="../assets/indeustry/Hospitality.png" 
-        placeholder="blur" 
-        alt="Hospitality" 
-        width={200} 
-        height={200} 
-      />
-    </div>
-    <div className="text-lg font-semibold">Hospitality</div>
-  </div>
-
-  <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
-    <div className="flex justify-center">
-      <Image 
-      src="../assets/indeustry/Pharmacy.png" 
-        placeholder="blur" 
-        alt="Pharmacy" 
-        width={200} 
-        height={200} 
-      />
-    </div>
-    <div className="text-lg font-semibold">Pharmacy</div>
-  </div>
-
-  <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
-    <div className="flex justify-center">
-      <Image 
-        src="../assets/indeustry/Ui-Ux.png" 
-        placeholder="blur" 
-        alt="UI/UX" 
-        width={200} 
-        height={200} 
-      />
-    </div>
-    <div className="text-lg font-semibold">UI/Ux</div>
-  </div>
-
-  <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
-    <div className="flex justify-center">
-      <Image 
-   src="../assets/indeustry/more.png" 
-        placeholder="blur" 
-        alt="Many more" 
-        width={200} 
-        height={200} 
-      />
-    </div>
-    <div className="text-lg font-semibold">Many more</div>
-  </div>
-</div>
+        <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
+          <div className="flex justify-center">
+            <Image src={healthcare} alt="About Tech It" placeholder="blur" />
+          </div>
+          <div className="text-lg font-semibold">Healthcare</div>
+        </div>
+        <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
+          <div className="flex justify-center">
+            <Image src={ecommerce} placeholder="blur" alt="About Tech It" />
+          </div>
+          <div className="text-lg font-semibold">E-Commerce</div>
+        </div>
+        <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
+          <div className="flex justify-center">
+            <Image src={education} placeholder="blur" alt="About Tech It" />
+          </div>
+          <div className="text-lg font-semibold">Education</div>
+        </div>
+        {/* <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
+          <div className="flex justify-center">
+            <Image src={RealEstate} placeholder="blur" alt="About Tech It" />
+          </div>
+          <div className="text-lg font-semibold">Real-Estate</div>
+        </div>
+        <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
+          <div className="flex justify-center">
+            <Image src={Hospitality} placeholder="blur" alt="About Tech It" />
+          </div>
+          <div className="text-lg font-semibold">Hospitality</div>
+        </div>
+        <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
+          <div className="flex justify-center">
+            <Image src={Pharmacy} placeholder="blur" alt="About Tech It" />
+          </div>
+          <div className="text-lg font-semibold">Pharmacy</div>
+        </div>
+        <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
+          <div className="flex justify-center">
+            <Image src={UIUx} placeholder="blur" alt="About Tech It" />
+          </div>
+          <div className="text-lg font-semibold">UI/Ux</div>
+        </div>
+        <div className="space-y-4 text-center rounded-lg bg-white p-6 shadow-lg dark:bg-[#18181B]">
+          <div className="flex justify-center">
+            <Image src={more} placeholder="blur" alt="About Tech It" />
+          </div>
+          <div className="text-lg font-semibold">Many more</div>
+        </div> */}
+      </div>
 
     </div>
   );
